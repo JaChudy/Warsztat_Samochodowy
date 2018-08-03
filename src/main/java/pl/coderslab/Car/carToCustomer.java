8789c3ee562fb3016265e2380f5b80ae86ae1db3
BartekFrac <fracu123@o2.pl>
2018-08-03 17:21:15 +0200

    car to user

---
+++ src/main/java/pl/coderslab/Car/carToCustomer.java
@@ -0,0 +1,36 @@
+package pl.coderslab.Car;
+
+import pl.coderslab.Customer.Customer;
+import pl.coderslab.Customer.CustomerDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/carToCustomer")
+public class carToCustomer extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String registrationNumber = request.getParameter("registrationNumber");
+
+        request.setAttribute("registrationNumber", registrationNumber);
+        getServletContext().getRequestDispatcher("/addCarToUser.jsp").forward(request,response);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Customer> customers = null;
+        try {
+            customers = CustomerDao.printAllCustomers();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        request.setAttribute("customers", customers);
+        getServletContext().getRequestDispatcher("/carToUser.jsp").forward(request,response);
+
+    }
+}
